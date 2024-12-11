1813a81f9ea5e1474e13868a67116a170f6207f4
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2024-12-11 17:47:11 -0400

    added props and useState with react

--- src/app/components/RandomFox.tsx
+++ src/app/components/RandomFox.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-// Generate a random function between 1 and 123.
-const random = () => Math.floor(Math.random() * 123) + 1;
-
-export const RandomFox = (): React.ReactElement => {
-    const image: string = `https://randomfox.ca/images/${random()}.jpg`;
-
+type Props = { image: string };
+export const RandomFox = ({image}: Props): React.ReactElement => {
     return <img
         width={320}
         height="auto"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import {RandomFox}  from "@/app/components/RandomFox";
+'use client';
+import {useState} from "react";
+import {RandomFox} from "@/app/components/RandomFox";
+
+// Generate a random function between 1 and 123.
+const random = () => Math.floor(Math.random() * 123) + 1;
 
 export default function Home() {
+    const [images, setImages] = useState<string[]>([
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`,
+        `https://randomfox.ca/images/${random()}.jpg`]);
+
     return (
         <div>
             <main>
                 <h1 className="text-3xl font-bold underline">Hello word Aldo!</h1>
-                <RandomFox/>
+                {images.map((image, index) => (
+                    <div key={index} className="p-4">
+                        <RandomFox image={image}/>
+                    </div>
+                ))}
+
             </main>
             <footer>
             </footer>
